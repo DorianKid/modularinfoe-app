5308c53154784000c6ba5a7021c73299002ed72c
Dario Niño <130793949+DorianKid@users.noreply.github.com>
2026-01-12 18:10:35 -0600

    Update 💭Foro.py

--- pages/💭Foro.py
+++ pages/💭Foro.py
@@ -1,11 +1,8 @@
 import streamlit as st
-from forum.db import init_db
 from forum.questions import create_question
 from forum.questions import list_questions
 
 st.set_page_config(page_title="Foro", layout="wide")
-
-init_db()
 
 st.title("💬 Foro Académico")
 
