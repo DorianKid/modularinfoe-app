32f52eae3bbe86f0f2943dcc7bbe0f16733cadd6
Dario Niño <130793949+DorianKid@users.noreply.github.com>
2026-01-12 18:20:17 -0600

    Update db.py

--- forum/db.py
+++ forum/db.py
@@ -1,14 +1,18 @@
 # forum/db.py
 import psycopg2
 import streamlit as st
+import socket
 
 def get_conn():
+    # 🔴 Fuerza IPv4 (clave)
+    socket.setdefaulttimeout(10)
     return psycopg2.connect(
         host=st.secrets["DB_HOST"],
         dbname=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
         password=st.secrets["DB_PASSWORD"],
-        port=st.secrets.get("DB_PORT", 5432),
-        sslmode="require"
+        port=int(st.secrets.get("DB_PORT", 5432)),
+        sslmode="require",
+        options="-c client_encoding=UTF8",
+        connect_timeout=10
     )
-
