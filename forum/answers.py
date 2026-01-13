efa1da597e084f0cf0c6114369e204b88277b492
Dario Niño <130793949+DorianKid@users.noreply.github.com>
2026-01-12 19:28:42 -0600

    Update answers.py

--- forum/answers.py
+++ forum/answers.py
@@ -21,7 +21,7 @@ def answers_section(question_id: int):
                 with st.container(border=True):
                     st.markdown(body, unsafe_allow_html=True)
 
-                    left, center, right = st.columns([4, 3, 3])
+                    left, center, right = st.columns([4, 3, 2])
                     
                     with center:
                         c1, c2 = st.columns(2)
@@ -82,6 +82,7 @@ def vote(answer_id: int, field: str):
     )
     conn.commit()
     conn.close()
+
 
 
 
